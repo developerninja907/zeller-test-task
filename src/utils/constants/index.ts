75a82240072083aad4411c9a0ea9897a24c8dc40
Pankaj <developerninja907@gmail.com>
2024-01-24 19:20:26 +0530

    completed task

---
+++ src/utils/constants/index.ts
@@ -0,0 +1,14 @@
+/**
+ * strings
+ */
+const STRING = {
+  USER_TYPE: 'User Types',
+  USER: 'User',
+  MANAGER: 'Manager',
+  ADMIN: 'Admin',
+  SEARCH_TEXT: 'Enter your search',
+};
+
+const RADIO_BUTTON_DIMENSIONS = 1.6;
+
+export {STRING, RADIO_BUTTON_DIMENSIONS};
